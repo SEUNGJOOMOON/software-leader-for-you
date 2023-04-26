7a93c3fdb191f95577bbd5b5e883520f137eb473
SEUNGJOOMOON <nick901106@gmail.com>
2023-04-26 21:21:32 +0900

    init

---
+++ src/main/java/nick/software/leaderForU/LeaderForUApplication.java
@@ -0,0 +1,13 @@
+package nick.software.leaderForU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderForUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderForUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nick/software/leaderForU/LeaderForUApplicationTests.java
@@ -0,0 +1,13 @@
+package nick.software.leaderForU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderForUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
