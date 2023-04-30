74b357e56f3f6a0ea271d4daeaf19eebccb6d0eb
nick901106 <nick901106@gmail.com>
2023-04-30 18:20:54 +0900

    ddd

--- src/main/java/com/test/mapper/BoardMapper.java
+++ src/main/java/com/test/mapper/BoardMapper.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.annotations.*;
 import java.util.List;
 
 @Mapper
-
 public interface BoardMapper {
 
     @Select("SELECT * FROM test_board")
--- src/main/java/nick/software/leaderForU/LeaderForUApplication.java
+++ src/main/java/nick/software/leaderForU/LeaderForUApplication.java
@@ -3,6 +3,7 @@ package nick.software.leaderForU;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -11,7 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 public class LeaderForUApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LeaderForUApplication.class, args);
+		SpringApplication app = new SpringApplication(LeaderForUApplication.class);
+		app.addListeners(new ApplicationPidFileWriter());   // pid 를 작성하는 역할을 하는 클래스 선언
+		app.run(args);
 	}
 
 }
